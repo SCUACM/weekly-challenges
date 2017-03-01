dd4400745047b02038c873ff6f47c4d48bef793e
Julian Callin <julian.callin@here.com>
2017-02-28 21:11:34 -0800

    Added optimized solution for week 7s problem (thanks Tim)

--- winter_2017/week_7_array_multiply/array_multiply.py
+++ winter_2017/week_7_array_multiply/array_multiply.py
@@ -19,7 +19,56 @@ def array_multiply(array):
 
         return new_array
 
+def array_multiply_efficient(array):
+        # [ a,     b,   c,    d,    e  ]
+
+        # We want an answer like
+        # [ bcde, a cde, ab de, abc e, abcd ]
+
+        # We can split the prefixes above into a second array
+        # Multiply the element of each together for the answer
+
+        # STEP 1
+        # partial = [bcde,  cde,  de,   e,    1  ]
+        # prefixes = [  1 ,  a,    ab,   abc, abcd]
+
+        # STEP 2
+        # result = [bcde*1, cde*a, de*ab, e*abc, 1*abcd]
+
+        partial = []
+        prefixes = []
+
+        # Make an array of lists of numbers to be multiplied together
+        for i in range(len(array)):
+                # Append a 1 at the end
+                if not array[i+1:]:
+                        partial.append([1])
+                        break
+                partial.append(array[i + 1:])
+
+        for i in range(len(array)):
+                # Append a 1 at the start
+                if not array[:i]:
+                        prefixes.append([1])
+                        continue
+                prefixes.append(array[:i])
+
+
+        # Multiply the elements together, as each element is a list of the
+        # numbers to multiply at this point
+        partial = [reduce(lambda x, y: x*y, r) for r in partial]
+        prefixes = [reduce(lambda x, y: x*y, r) for r in prefixes]
+
+        # Now multiply each element in the partials list by its corresponding
+        # element in the partials list
+        result = [x * y for x, y in zip(partial, prefixes)]
+
+        return result
+
 
 if __name__ == "__main__":
         print(array_multiply([1,2,3,4,5]))
+        print(array_multiply_efficient([1,2,3,4,5]))
+
+
 
